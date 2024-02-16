6c8d037fc96f4757e697dcf8c3a38ba27c4c8d28
insdout <insdxout@gmail.com>
2024-02-16 23:28:56 +0200

    major update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 from PIL import Image
 from src.model import get_outputs
-from src.plots import plot_all_attentions, plot_attention, plot_top_k, is_mask
+from src.plots import plot_all_attentions, plot_attention, plot_top_k, is_mask, remove_special_tokens
+import numpy as np
 
 
 def main():
@@ -19,9 +18,6 @@ def main():
 
     c1, c2 = st.columns([0.32, 2])
 
-    # The snowflake logo will be displayed in the first column, on the left.
-
-    # The snowflake logo will be displayed in the first column, on the left.
     with c1:
         st.write(" ")
         st.write(" ")
@@ -30,46 +26,50 @@ def main():
             width=60,
         )
 
-    # The heading will be on the right.
     with c2:
-        st.title("Bert Attention Weight Visualizer")
+        st.title("DistilBERT Attention Weights")
 
     # Add a text input box to the sidebar with default text
     default_text = 'Attention is [MASK] you need.'
 
+    st.sidebar.title('Controls:')
     with st.sidebar.form(key='input'):
-        text_input = st.text_input(label='Enter some text', value=default_text, max_chars=500)
+        text_input = st.text_input(label='Input sentence:', value=default_text, max_chars=200)
         submit_button = st.form_submit_button(label='Submit')
-    
+
+    with st.sidebar.container(border=True):
+        st.write('Special Tokens:')
+        include_special_tokens = st.checkbox('Include Special Tokens')
+
+    with st.sidebar.container(border=True):
+        # Dropdowns to select layer and head
+        num_layers = 6
+        num_heads = 12
+        st.write('Attention Head plot settings:')
+        layer = st.selectbox("Select layer:", range(1, num_layers + 1)) - 1
+        head = st.selectbox("Select head:", range(1, num_heads + 1)) - 1
+        attn_fig_size = st.slider('Attention figure size', min_value=1.0, max_value=6.0, step=0.1, value=2.0)
+
+    with st.sidebar.container(border=True):
+        st.write('TopK Head plot settings:')
+        topk_fig_size = st.slider('TopK figure size', min_value=1.0, max_value=6.0, step=0.1, value=2.0)
 
     outputs = get_outputs(text_input)
-    # Plot all attentions
-    st.subheader("Max Attention Weight")
-    plot_all_attentions(outputs)
+    special_tokens_removed, mask = remove_special_tokens(outputs)
+    if outputs is not None and np.any(mask):
+        # Plot all attentions
+        st.subheader("Max Attention Weights:")
+        plot_all_attentions(outputs, include_special_tokens=include_special_tokens)
 
-    # Dropdowns to select layer and head
-    num_layers = 6
-    num_heads = 12
+        # Plot attention for the selected layer and head
+        st.subheader(f"Attention for Layer {layer + 1}, Head {head + 1}:")
+        plot_attention(outputs, layer, head, include_special_tokens=include_special_tokens, figsize=attn_fig_size)
 
-    c21, c22 = st.columns([1, 1])
-
-    with c21:
-        layer = st.selectbox("Select layer:", range(num_layers))
-        include_special_tokens = st.checkbox('Include Special Tokens')
-    with c22:
-        head = st.selectbox("Select head:", range(num_heads))
-        attn_fig_size = st.slider('Figure size', min_value=1, max_value=6, step=1, value=3)
-
-    # Plot attention for the selected layer and head
-    st.subheader(f"Attention for Layer {layer}, Head {head}")
-    plot_attention(outputs, layer, head, include_special_tokens=include_special_tokens, figsize=attn_fig_size)
-    
-    if is_mask(outputs):
-        c31, c32 = st.columns([1, 1])
-        with c31:
-            top_k_fig_size = st.slider('TopK figure size', min_value=1, max_value=6, step=1, value=3)
-        plot_top_k(outputs, figsize=top_k_fig_size)
-    st.write(text_input)
+        if is_mask(outputs):
+            st.subheader("TopK predictions for [MASK] tokens:")
+            plot_top_k(outputs, figsize=topk_fig_size)
+    else:
+        st.error("Failed to get outputs. Please check your input and try again.")
 
 
 if __name__ == "__main__":
